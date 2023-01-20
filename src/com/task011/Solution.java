5662985bb91dd845362f9d56dd070bd2d95427ef
Serge Kravchenko <0634380033@ukr.net>
2023-01-20 21:01:53 +0200

    task011
    task011

---
+++ src/com/task011/Solution.java
@@ -0,0 +1,9 @@
+package com.task011;
+
+public class Solution {
+    public static void main(String[] args) {
+        StackDemo demo = new StackDemo();
+        demo.addToStack();
+        demo.getFromStack();
+    }
+}
