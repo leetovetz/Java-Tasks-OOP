4b2c0b6c7c6ff439594b976fb3fd6df6caf220aa
Serge Kravchenko <0634380033@ukr.net>
2023-01-21 20:47:20 +0200

    task016
    task016

---
+++ src/com/task016/Solution.java
@@ -0,0 +1,9 @@
+package com.task016;
+
+public class Solution {
+    public static void main(String[] args) {
+        Vocabulary vocabulary = new Vocabulary();
+        vocabulary.addNewWords();
+        vocabulary.getFromFile();
+    }
+}
