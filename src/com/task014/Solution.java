4ebe6840d6c0b31c4e46ed51955dbb0794e6403b
Serge Kravchenko <0634380033@ukr.net>
2023-01-21 20:28:38 +0200

    task014
    task014

---
+++ src/com/task014/Solution.java
@@ -0,0 +1,8 @@
+package com.task014;
+
+public class Solution {
+    public static void main(String[] args) {
+        DoubleCola cola = new DoubleCola();
+        System.out.println(cola.getDoubleCola(2));
+    }
+}
