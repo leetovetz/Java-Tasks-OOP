68467b6cfb97b23bad022afb308b5d48d306c9bd
Serge Kravchenko <0634380033@ukr.net>
2023-02-03 21:42:54 +0200

    task021
    task021

---
+++ src/com/task021/Solution.java
@@ -0,0 +1,23 @@
+package com.task021;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Solution {
+    @TestOne(a = 2, b = 5)
+    public static int test(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Class<?> cls = Solution.class;
+        Method[] methods = cls.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(TestOne.class)) {
+                TestOne testOne = method.getAnnotation(TestOne.class);
+                int result = (Integer) method.invoke(null, testOne.a(), testOne.b());
+                System.out.println(result);
+            }
+        }
+    }
+}
---
+++ src/com/task021/TestOne.java
@@ -0,0 +1,13 @@
+package com.task021;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface TestOne {
+    int a();
+    int b();
+}
